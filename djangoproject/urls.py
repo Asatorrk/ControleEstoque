977ba29a6de1d203720b089f2e401d0c2d185fdd
Rafael Zanco <136736512+Asatorrk@users.noreply.github.com>
2024-05-16 17:00:39 -0300

    ok

---
+++ djangoproject/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for djangoproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from stockmgmgt import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+	path('', views.home, name='home'),
+	path('list_items/', views.list_items, name='list_items'),
+	path('add_items/', views.add_items, name='add_items'),
+    path('update_items/<str:pk>/', views.update_items, name="update_items"),
+    path('delete_items/<str:pk>/', views.delete_items, name="delete_items"),
+    path('stock_detail/<str:pk>/', views.stock_detail, name="stock_detail"),
+    path('issue_items/<str:pk>/' , views.issue_items, name="issue_items"),
+    path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),
+    path('reorder_level/<str:pk>/', views.reorder_level, name="reorder_level"),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.default.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+]   
