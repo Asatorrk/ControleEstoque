6083f19783a1084dfc450ca80bd176d01042d1dc
Rafael Zanco <136736512+Asatorrk@users.noreply.github.com>
2024-10-30 06:29:53 -0300

    Add files via upload

--- stockmgmgt/models.py
+++ stockmgmgt/models.py
@@ -12,8 +12,8 @@ category_choice = (
 
 
 class Stock(models.Model):
-	category = models.CharField(max_length=50, blank=True, null=True, choices=category_choice)
-	item_name = models.CharField(max_length=50, blank=True, null=True)
+	category = models.CharField(max_length=50, blank=False, null=True)
+	item_name = models.CharField(max_length=50, blank=False, null=True)
 	quantity = models.IntegerField(default='0', blank=False, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_by = models.CharField(max_length=50, blank=True, null=True)
@@ -24,10 +24,10 @@ class Stock(models.Model):
 	created_by = models.CharField(max_length=50, blank=True, null=True)
 	reorder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	expiration_date = models.DateField(blank=True, null=True)  # Adicionando o campo da data de validade
+	expiration_date = models.DateField(blank=False, null=True)  # Adicionando o campo da data de validade
 	#timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	#date = models.DateTimeField(auto_now_add=False, auto_now=False)
-	
+
 
 	def __str__(self):
 		return self.item_name + ' ' + str(self.quantity)
